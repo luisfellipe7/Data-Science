46e2779efa64c063eb4815f3a5c2aeb6afbd60d5
Luis Fellipe Furio Raphael <lfraphael@gmail.com>
2018-01-31 13:23:17 -0200

    Add files via upload

---
+++ Melbourne  House Data/MelburneHouseData.r
@@ -0,0 +1,28 @@
+# load in packages we'll use
+library(tidyverse) # utility functions
+library(rpart) # for regression trees
+library(randomForest) # for random forests
+
+# read the data and store data in DataFrame titled melbourne_data
+melbourne_data <- read_csv("C:/Users/lfrap/Documents/R Course/Melbourne  House Data/melb_data.csv")
+
+#printing a summary of the data
+summary(melbourne_data)
+
+#building a decision tree based on the dataset
+fit <- rpart(Price ~ Rooms + Bathroom + Landsize + BuildingArea +
+               YearBuilt + Lattitude + Longtitude, data = melbourne_data)
+
+#looking at the tree and making it bigger
+plot(fit, uniform=TRUE)
+text(fit, cex=.6)
+
+#Now the fitted model can be used to predict house prices
+print("Making predictions for the following 5 houses:")
+print(head(melbourne_data))
+
+print("The predictions are")
+print(predict(fit, head(melbourne_data)))
+
+print("Actual price")
+print(head(melbourne_data$Price))
